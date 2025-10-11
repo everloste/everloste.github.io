908dc1c14d4aac3bac2c30e77fdd44f2a5878a19
Bee <117178732+everloste@users.noreply.github.com>
2025-10-11 23:03:39 +0200

    blog updates

--- src/lib/components/blog-posts.svelte
+++ src/lib/components/blog-posts.svelte
@@ -5,8 +5,23 @@
 <div>
 	{#each data.posts as post}
 		<a href={post.path} class="unformatted">
+			{#if post.meta.thumbnailstyle}
+				<img 
+					src={post.meta.thumbnail}
+					alt="Purely for decoration, can be ignored."
+					style={post.meta.thumbnailstyle}
+				/>
+			{:else if post.meta.thumbnail}
+				<img 
+					src={post.meta.thumbnail}
+					alt="Purely for decoration, can be ignored."
+				/>
+			{/if}
+			<h4>
+				{post.meta.title}
+			</h4>
 			<p>
-				<b>{post.meta.title}</b>
+				{post.meta.summary}
 			</p>
 			<p style="text-align: right;">
 				{new Date(post.meta.date).toLocaleDateString("en-IE", {weekday: "long", year: "numeric", month: "short", day: "2-digit",})}
@@ -32,6 +47,25 @@
 				color: var(--accent-color);
 				background-color: var(--background-color-brightest);
 				border: solid 1px var(--accent-color);
+				> img {
+					filter: brightness(1.1) sepia(25%);
+					height: 8em;
+				}
+			}
+			> p {
+				margin: 0;
+			}
+			> h4 {
+				margin-top: 0;
+			}
+			> img + h4 {
+				margin-top: 1em;
+			}
+			> img {
+				transition: all 1s var(--ease-out-perfect);
+				width: 100%;
+				height: 7em;
+				object-fit: cover;
 			}
 		}
 	}
