b859288c43e65e250d0e43fe083e3aaee072c104
Bee <117178732+everloste@users.noreply.github.com>
2025-10-11 14:20:53 +0200

    update nav

--- src/lib/components/site-nav.svelte
+++ src/lib/components/site-nav.svelte
@@ -1,8 +1,6 @@
 <span>
 	<a href="/">home</a>
 	&lowast; <a href="/blog">blog</a>
-	&lowast; <a href="/about">about</a>
-	&lowast; <a href="/about">my work</a>
-	&lowast; <a href="/my-projects">mc projects</a>
+	&lowast; <a href="/mc-projects">mc projects</a>
 	&lowast; <a href="/ko-fi">support me</a>
 </span>
